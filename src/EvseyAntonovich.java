de719a97e44a31814a3c806471214e032c3f2d32
evsey9 <evseyru1@mail.ru>
2022-10-22 22:14:49 +0300

    Finished the AStar algo implementation, changed coordinate output, improved map/path visualisation, added more map helper functions, made an algorithm output formatter

--- src/EvseyAntonovich.java
+++ src/EvseyAntonovich.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.*;
 
 /**
@@ -100,7 +101,7 @@ class Point {
 
     @Override
     public String toString() {
-        return "{" + x + "," + y + "}";
+        return "[" + y + "," + x + "]";
     }
 }
 
@@ -292,7 +293,7 @@ class Map {
      * @param path The path to overlay on the map, null for no path overlay
      * @return String representation, including linebreaks
      */
-    public String getStringVisualization(boolean overlayPerception, List<Point> path) {
+    public String getStringVisualization(boolean overlayPerception, PathResult path) {
         List<List<Character>> lines = new ArrayList<>(mapSize);
         for (int i = 0; i < mapSize; i++) {
             List<Character> curList = new ArrayList<Character>();
@@ -318,25 +319,30 @@ class Map {
         lines.get(chestLocation.y).set(chestLocation.x, '#');
         lines.get(tortugaLocation.y).set(tortugaLocation.x, '$');
 
+        char lastChar = '.';
         if (path != null) {
-            for (int i = 1; i < path.size() - 1; i++) {
-                Point curPoint = path.get(i);
-                Point prevPoint = path.get(i - 1);
-                Point nextPoint = path.get(i + 1);
+            for (int i = 1; i < path.path.size() - 1; i++) {
+                Point curPoint = path.path.get(i);
+                Point prevPoint = path.path.get(i - 1);
+                Point nextPoint = path.path.get(i + 1);
                 int diffX = nextPoint.x - prevPoint.x;
                 int diffY = nextPoint.y - prevPoint.y;
                 if (diffX > 0 && diffY > 0 || diffX < 0 && diffY < 0) {
-                    lines.get(curPoint.y).set(curPoint.x, '\\');
+                    lines.get(curPoint.y).set(curPoint.x, '⟍');
                 }
                 if (diffX > 0 && diffY < 0 || diffX < 0 && diffY > 0) {
-                    lines.get(curPoint.y).set(curPoint.x, '/');
+                    lines.get(curPoint.y).set(curPoint.x, '⟋');
                 }
                 if (diffX == 0 && diffY != 0) {
                     lines.get(curPoint.y).set(curPoint.x, '|');
                 }
                 if (diffY == 0 && diffX != 0) {
-                    lines.get(curPoint.y).set(curPoint.x, '-');
+                    lines.get(curPoint.y).set(curPoint.x, '—');
                 }
+                if (diffX == 0 && diffY == 0 && lastChar != '.') {
+                    lines.get(curPoint.y).set(curPoint.x, lastChar);
+                }
+                lastChar = lines.get(curPoint.y).get(curPoint.x);
             }
         }
 
@@ -344,10 +350,62 @@ class Map {
         for (int i = 0; i < mapSize; i++) {
             for (int j = 0; j < mapSize; j++) {
                 outStrBuilder.append(lines.get(i).get(j));
+                outStrBuilder.append(' ');
             }
             outStrBuilder.append('\n');
         }
+        outStrBuilder.setLength(outStrBuilder.length() - 1);
         return outStrBuilder.toString();
+    }
+
+    /**
+     * Gets the neighbors that you can move into from the point origin
+     * @param origin Point to find the neighbors of
+     * @param krakenPresent Whether the Kraken is alive or not
+     * @return List of neighbor points
+     */
+    List<Point> getNeighbors(Point origin, boolean krakenPresent) {
+        List<Point> neighbors = new ArrayList<>();
+        for (int i = origin.y - 1; i <= origin.y + 1; i++) {
+            if (i < 0 || i >= mapSize) {  // Checking if y is within bounds
+                continue;
+            }
+            for (int j = origin.x - 1; j <= origin.x + 1; j++) {
+                if (j < 0 || j >= mapSize) {  // Checking if x is within bounds
+                    continue;
+                }
+                if (i == origin.y && j == origin.x) {  // Checking that we are not at the origin, as it is not a neighbour
+                    continue;
+                }
+                if (perceptionZone.get(i).get(j) || krakenPresent && krakenZone.get(i).get(j)) {  // Checking that the point is not within a perception zone
+                    continue;
+                }
+                Point neighbor = new Point();
+                neighbor.x = j;  // Do this instead of using the constructor because we may swap the y and x arguments in the constructor at one point
+                neighbor.y = i;
+                neighbors.add(neighbor);
+            }
+        }
+        return neighbors;
+    }
+
+    /**
+     * Checks whether the specified point is within map bounds
+     * @param point Point to check the validity of
+     * @return True if point is within map bounds, false otherwise
+     */
+    boolean pointWithinBounds(Point point) {
+        return point.x >= 0 && point.x < mapSize && point.y >= 0 && point.y < mapSize;
+    }
+
+    /**
+     * Checks whether the specified point is on a perception zone or not
+     * @param point Point to check
+     * @param krakenPresent Whether the kraken is alive or not
+     * @return True if point is on a perception zone, false otherwise
+     */
+    boolean pointOnPerceptionZone(Point point, boolean krakenPresent) {
+        return perceptionZone.get(point.y).get(point.x) || krakenPresent && krakenZone.get(point.y).get(point.x);
     }
 }
 
@@ -362,7 +420,7 @@ class MapInput {
     public Point rockCoord;
     public Point chestCoord;
     public Point tortugaCoord;
-    MapInput() {};
+    MapInput() {}
     MapInput(int scenario, Point captainCoord, Point davyCoord, Point krakenCoord, Point rockCoord, Point chestCoord, Point tortugaCoord) {
         this.scenario = scenario;
         this.captainCoord = captainCoord;
@@ -518,6 +576,11 @@ class FileLinesReader {
     FileLinesReader() {
         this("input.txt");
     }
+
+    /**
+     * Gets a list of lines from the specified filename
+     * @return List of lines
+     */
     public List<String> getLines() {
         List<String> output = new ArrayList<>();
         try {
@@ -539,12 +602,33 @@ class FileLinesReader {
  */
 class AlgorithmOutput {
     double millisecondRuntime;
-    List<Point> path;
-    AlgorithmOutput(double millisecondRuntime, List<Point> path) {
+    PathResult path;
+    AlgorithmOutput(double millisecondRuntime, PathResult path) {
         this.millisecondRuntime = millisecondRuntime;
         this.path = path;
     }
-    AlgorithmOutput() {};
+    AlgorithmOutput() {}
+}
+
+/**
+ * Class for the result of a pathfinding algorithm (start, end and the path itself)
+ */
+class PathResult {
+    public Point start;
+    public Point end;  // We need this as the path can end *somewhere* near the goal
+    public Point goal;
+    public List<Point> path;
+
+    @Override
+    public String toString() {
+        StringBuilder outStr = new StringBuilder();
+        for (Point p : path) {
+            outStr.append(p.toString());
+            outStr.append(' ');
+        }
+        outStr.setLength(outStr.length() - 1);
+        return outStr.toString();
+    }
 }
 
 /**
@@ -573,23 +657,211 @@ abstract class Algorithm {
 
     /**
      * Gets the shortest path from Jack Sparrow to the Dead Man's Chest
-     * @return List of points that are part of the path
+     * @return PathResult that contains the list of points that are part of the path
      */
-    abstract protected List<Point> getPathBody();
+    abstract protected PathResult getPathBody();
 }
 
+/**
+ * Class for the A* algorithm
+ */
 class AStarAlgorithm extends Algorithm {
+    /**
+     * Class for a single A* grid tile
+     */
+    class AStarTile {
+        int g = -1;
+        int h = -1;
+        int f = -1;
+        Point location = null;
+        AStarTile parent = null;
+        AStarTile(Point location) {
+            this.location = location;
+        }
+        AStarTile() {}
+
+        /**
+         * Updates the costs of an A* tile
+         * @param neighbor The neighbor from which the cost is being updated
+         * @param cost Cost of movement to this tile
+         * @param target Goal point for computing h cost
+         */
+        public void updateCosts(AStarTile neighbor, int cost, Point target) {
+            h = location.diagonalDistance(target, 1, 1);
+            if (neighbor != null) {
+                if (g == -1 || neighbor.g + cost < g) {
+                    g = neighbor.g + cost;
+                    parent = neighbor;
+                }
+            } else {
+                g = 0;
+            }
+            f = g + h;
+        }
+    }
     AStarAlgorithm(Map map, int perceptionType) {
         super(map, perceptionType);
     }
 
+    boolean krakenDiscovered = false;
+
     /**
      * Gets the shortest path from Jack Sparrow to the Dead Man's Chest using A*
-     * @return List of points that are part of the path
+     * @return PathResult that contains the list of points that are part of the path
      */
     @Override
-    protected List<Point> getPathBody() {
+    protected PathResult getPathBody() {
+        PathResult shortestPath = getPathBetweenPoints(map.captainLocation, map.chestLocation, 0, true);
+        if (krakenDiscovered) {
+            PathResult toTortuga = getPathBetweenPoints(map.captainLocation, map.tortugaLocation, 0, true);
+            toTortuga.path.remove(toTortuga.path.size() - 1);
+            List<Point> krakenPoints = Arrays.asList(
+                    map.krakenLocation.sum(new Point(-1, -1)),
+                    map.krakenLocation.sum(new Point(1, -1)),
+                    map.krakenLocation.sum(new Point(1, 1)),
+                    map.krakenLocation.sum(new Point(-1, 1)));
+            List<PathResult> toKrakenPaths = new ArrayList<>(4);
+            for (int i = 0; i < 4; i++) {
+                Point curKrakenPoint = krakenPoints.get(i);
+                if (map.pointWithinBounds(curKrakenPoint) && !map.pointOnPerceptionZone(curKrakenPoint, true)) {
+                    PathResult curPath = getPathBetweenPoints(map.tortugaLocation, krakenPoints.get(i), 0, true);
+                    if (curPath != null) {
+                        curPath.path.remove(curPath.path.size() - 1);
+                        toKrakenPaths.add(curPath);
+                    }
+                }
+            }
+            List<PathResult> totalPaths = new ArrayList<>(4);
+            for (PathResult toKrakenPath : toKrakenPaths) {
+                Point curKrakenPoint = toKrakenPath.end;
+                PathResult curPathAfter = getPathBetweenPoints(curKrakenPoint, map.chestLocation, 0, false);
+                if (curPathAfter != null) {
+                    PathResult totalPath = new PathResult();
+                    totalPath.start = map.captainLocation;
+                    totalPath.end = map.chestLocation;
+                    totalPath.goal = map.chestLocation;
+                    List<Point> curActualPath = new ArrayList<>();
+                    curActualPath.addAll(toTortuga.path);
+                    curActualPath.addAll(toKrakenPath.path);
+                    curActualPath.addAll(curPathAfter.path);
+                    totalPath.path = curActualPath;
+                    totalPaths.add(totalPath);
+                }
+            }
+            PathResult minPath = null;
+            for (PathResult curPath : totalPaths) {
+                if (curPath != null && (minPath == null || curPath.path.size() < minPath.path.size())) {
+                    minPath = curPath;
+                }
+            }
+            if (minPath != null && (shortestPath == null || minPath.path.size() < shortestPath.path.size())) {
+                shortestPath = minPath;
+            }
+        }
+        return shortestPath;
+    }
+
+    /**
+     * Gets path between point start and point end, with distance being the distance between the possible end point and actual goal
+     * @param start Starting point
+     * @param end Goal/end point
+     * @param distance Diagonal distance to the end point, reaching which the algorithm stops
+     * @param krakenPresent Whether to consider the Kraken alive or not
+     * @return PathResult containing the path and start, end, goal points, null if no path found
+     */
+    protected PathResult getPathBetweenPoints(Point start, Point end, int distance, boolean krakenPresent) {
+        PathResult result = new PathResult();
+        result.start = start;
+        result.goal = end;
+        List<Point> path = new ArrayList<>();
+        Set<AStarTile> closed = new HashSet<>();
+        List<AStarTile> open = new ArrayList<>();
+        List<List<AStarTile>> tiles = new ArrayList<>(map.mapSize);
+        for (int i = 0; i < map.mapSize; i++) {
+            List<AStarTile> curList = new ArrayList<>(map.mapSize);
+            for (int j = 0; j < map.mapSize; j++) {
+                curList.add(new AStarTile(new Point(j, i)));
+            }
+            tiles.add(curList);
+        }
+        open.add(tiles.get(start.y).get(start.x));
+        open.get(0).updateCosts(null, 0, end);
+        while (open.size() > 0) {
+            AStarTile current = null;
+            int chosenI = -1;
+            for (int i = 0; i < open.size(); i++) {  // Pick tile with the lowest cost
+                AStarTile curOpen = open.get(i);
+                if (current == null || open.get(i).f < current.f ||
+                        curOpen.f == current.f && curOpen.h < current.h ||
+                        curOpen.f == current.f && curOpen.h == current.h && curOpen.location.manhattanDistance(end, 1) < current.location.manhattanDistance(end, 1)) {
+                    // First we check if the total cost is less, then, we check if the total cost is equal but the heuristic cost is smaller (so it's closer).
+                    // Then, if both total cost and equal cost are equal, we check if the manhattan distance is smaller than the current manhattan distance.
+                    // As manhattan distance is a direct difference of coordinates, this will prioritize paths that try to minimize the coordinate difference
+                    // instead of paths that may also be the shortest, but look slightly less logical, due to Chebyshev cost heuristics.
+                    chosenI = i;
+                    current = open.get(i);
+                }
+            }
+            open.remove(chosenI);
+            Point curLoc = current.location;
+            if (perceptionType == 1 && curLoc.diagonalDistance(map.krakenLocation, 1, 1) == 1 || perceptionType == 2 && curLoc.manhattanDistance(map.krakenLocation, 1) <= 2) {
+                krakenDiscovered = true;
+            }
+            if (curLoc.diagonalDistance(end, 1, 1) == distance) {  // We are close enough to the goal to finish
+                List<Point> outputPath = new ArrayList<>();
+                result.end = curLoc;
+                AStarTile curTile = current;
+                while (curTile != null) {
+                    outputPath.add(0, curTile.location);
+                    curTile = curTile.parent;
+                }
+                result.path = outputPath;
+                return result;
+            }
+            closed.add(current);
+            List<Point> pointNeighbors = map.getNeighbors(curLoc, krakenPresent);
+            for (int i = 0; i < pointNeighbors.size(); i++) {
+                Point pointNeighbor = pointNeighbors.get(i);
+                AStarTile neighborTile = tiles.get(pointNeighbor.y).get(pointNeighbor.x);
+                if (closed.contains(neighborTile)) {
+                    continue;
+                }
+                neighborTile.updateCosts(current, 1, end);
+                if (!open.contains(neighborTile)) {
+                    open.add(neighborTile);
+                }
+            }
+        }
         return null;
+    }
+}
+
+/**
+ * Class to format the algorithm results
+ */
+class AlgorithmResultFormatter {
+    Map map;
+    AlgorithmOutput algorithmOutput;
+    AlgorithmResultFormatter(Map map, AlgorithmOutput algorithmOutput) {
+        this.map = map;
+        this.algorithmOutput = algorithmOutput;
+    }
+
+    /**
+     * Makes a string that is valid output for the algorithm, including Win/Lose, shortest distance, the shortest path itself, and path visualized on the map
+     * @return The string
+     */
+    String makeString() {
+        StringBuilder outStr = new StringBuilder();
+        outStr.append("Win\n");
+        outStr.append(algorithmOutput.path.path.size() - 1);
+        outStr.append('\n');
+        outStr.append(algorithmOutput.path);
+        outStr.append('\n');
+        outStr.append(map.getStringVisualization(true, algorithmOutput.path));
+        outStr.append('\n');
+        outStr.append(algorithmOutput.millisecondRuntime).append(" ms\n");
+        return outStr.toString();
     }
 }
 
@@ -603,26 +875,18 @@ public class EvseyAntonovich {
         System.out.println("Enter 1 for input.txt input, enter 2 for random map generation");
         Scanner myScanner = new Scanner(System.in);
         n = myScanner.nextInt();
+        int perceptionType = 1;
         MapInput myInput;
         List<Point> path = null;
         if (n == 1) {
-            FileLinesReader reader = new FileLinesReader();
+            FileLinesReader reader = new FileLinesReader("input1.txt");
             myInput = InputParser.parseLines(reader.getLines());
             if (myInput == null) {
                 System.out.println("Invalid input! Please enter valid input.");
+                return;
+            } else {
+                perceptionType = myInput.scenario;
             }
-            path = new ArrayList<>();
-            path.add(new Point(0, 0));
-            path.add(new Point(0, 1));
-            path.add(new Point(0, 2));
-            path.add(new Point(1, 3));
-            path.add(new Point(2, 4));
-            path.add(new Point(3, 4));
-            path.add(new Point(4, 4));
-            path.add(new Point(5, 4));
-            path.add(new Point(6, 5));
-            path.add(new Point(7, 6));
-            path.add(new Point(8, 7));
         } else {
             myInput = null;
         }
@@ -631,6 +895,14 @@ public class EvseyAntonovich {
             System.out.println("Invalid map! Please restart program.");
             return;
         }
-        System.out.println(myMap.getStringVisualization(true, null));
+        AStarAlgorithm astarAlgo = new AStarAlgorithm(myMap, perceptionType);
+        AlgorithmOutput output = astarAlgo.getPath();
+        AlgorithmResultFormatter formatterAstar = new AlgorithmResultFormatter(myMap, output);
+        try (PrintWriter astarWriter = new PrintWriter("outputAStar.txt")) {
+            astarWriter.print(formatterAstar.makeString());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(formatterAstar.makeString());
     }
 }
